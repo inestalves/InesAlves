94fc640b094310f289f7b06874a17cbf0d2470cc
inestalves <inestripeca6@gmail.com>
2024-12-06 10:12:17 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='InesAlves',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='Inês Alves',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['InesAlves'],
+     )
